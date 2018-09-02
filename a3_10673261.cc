02dd184b2195ff0a77bd64faa49a8471b3e8954d
perez-yeboah <42807018+perez-yeboah@users.noreply.github.com>
2018-09-02 20:09:43 +0000

    Update a3_10673261.cc

--- a3_10673261.cc
+++ a3_10673261.cc
@@ -9,31 +9,31 @@ int main() {
     cout << "Enter your mark" << endl;
     cin >> mrk;
 
-    if(mark >= 80 && mrk <= 100)
+    if(mak >= 80 && mrk <= 100)
     {
         grd = "A";
-    } else if(mark >= 75 && mrk <= 79)
+    } else if(mrk >= 75 && mrk <= 79)
     {
         grd = "B+";
-    } else if(mark >= 70 && mrk <= 74)
+    } else if(mrk >= 70 && mrk <= 74)
     {
         grd = "B";
-    } else if(mark >= 65 && mrk <= 69)
+    } else if(mrk >= 65 && mrk <= 69)
     {
         grd = "C+";
-    } else if(mark >= 60 && mrk <= 64)
+    } else if(mrk >= 60 && mrk <= 64)
     {
         grd = "C";
-    } else if(mark >= 55 && mrk <= 59)
+    } else if(mrk >= 55 && mrk <= 59)
     {
         grd = "D+";
-    } else if(mark >= 50 && mrk <= 54)
+    } else if(mrk >= 50 && mrk <= 54)
     {
         grd = "D";
-    } else if(mark >= 45 && mrk <= 49)
+    } else if(mrk >= 45 && mrk <= 49)
     {
         grd = "E";
-    } else if(mark <= 44 && mrk >= 0)
+    } else if(mrk <= 44 && mrk >= 0)
     {
         grd = "F";
     } else
